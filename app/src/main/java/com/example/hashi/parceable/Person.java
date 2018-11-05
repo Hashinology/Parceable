3789d671a727dde87701b3a6ae0a599ed29c6611
Hashi <m.sharif.uk@gmail.com>
2018-11-05 12:05:15 +0000

    First Comment

---
+++ app/src/main/java/com/example/hashi/parceable/Person.java
@@ -0,0 +1,74 @@
+package com.example.hashi.parceable;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Person implements Parcelable
+    {
+
+        private String name;
+        private String gender;
+
+        public Person(String name, String gender)
+        {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        public String getName() {
+
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getGender() {
+
+            return gender;
+        }
+
+        public void setGender(String gender) {
+            this.gender = gender;
+        }
+
+        /*public void Person(String name, String gender)
+        {
+            this.name = name;
+            this.gender = gender;
+        } */
+
+        protected Person(Parcel in) {
+            name = in.readString();
+            gender = in.readString();
+        }
+
+        public static final Creator<Person> CREATOR = new Creator<Person>() {
+            @Override
+            public Person createFromParcel(Parcel in) { return new Person(in); }
+
+            @Override
+            public Person[] newArray(int size) { return new Person[size]; }
+
+        };
+
+        @Override
+        public int describeContents() {  return 0; }
+
+
+        @Override
+        public void writeToParcel(Parcel parcel, int i) {
+
+            parcel.writeString(name);
+            parcel.writeString(gender);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender='" + gender + '\'' +
+                    '}';
+        }
+    }
